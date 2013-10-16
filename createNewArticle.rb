009db208c20207e065200fe1d76c0ea51eb76374
naosim <nao_pillows@yahoo.co.jp>
2013-10-17 07:09:17 +0900

    デグレ修正

--- createNewArticle.rb
+++ createNewArticle.rb
@@ -7,15 +7,28 @@ NEW_ARTICLE_FILE_PATH = "workspace/newarticle.txt"
 BIN_ARTICLE_DIR = "bin/article/"
 #----
 
+def getArticleDataFileNameList(articleDir)
+	current = Dir.pwd
+	Dir.chdir(articleDir)
+	result = Dir.glob("*")
+	Dir.chdir(current)
+
+	return result.reverse
+end
+
 class ArticleNameDetector
 	FILE_FORMAT = "%04d.dat"
-	def initialize articles	
-		@articles = articles
+	def initialize articleDir	
+		current = Dir.pwd
+		Dir.chdir(articleDir)
+		@length = Dir.glob("*").length
+		Dir.chdir(current)
 	end
+
 	def getNewName
-		return sprintf(FILE_FORMAT, @articles.length)
+		return sprintf(FILE_FORMAT, @length)
 	end
 end
 
-articleName = ArticleNameDetector.new(Articles.new(BIN_ARTICLE_DIR)).getNewName
+articleName = ArticleNameDetector.new(BIN_ARTICLE_DIR).getNewName
 File.copy(NEW_ARTICLE_FILE_PATH, BIN_ARTICLE_DIR + articleName)
